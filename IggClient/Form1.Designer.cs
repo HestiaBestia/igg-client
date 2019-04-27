0505980cf30ec02428cad2f2652315000cd2f471
HestiaBestia <50001439+HestiaBestia@users.noreply.github.com>
2019-04-27 04:27:09 -0500

    Add files via upload

--- IggClient/Form1.Designer.cs
+++ IggClient/Form1.Designer.cs
@@ -33,22 +33,44 @@
             this.unresolvedBox = new System.Windows.Forms.TextBox();
             this.resolvedBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.torrentLabel2 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            this.resolverButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.directdlButton = new System.Windows.Forms.Button();
+            this.torrentButton = new System.Windows.Forms.Button();
+            this.hestia = new System.Windows.Forms.PictureBox();
+            this.credits1 = new System.Windows.Forms.Label();
+            this.credits2 = new System.Windows.Forms.Label();
+            this.torrentUrl = new System.Windows.Forms.TextBox();
+            this.magnetLink = new System.Windows.Forms.TextBox();
+            this.torrentLabel1 = new System.Windows.Forms.Label();
+            this.magnetgrabButton = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.hestia)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(267, 297);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(278, 334);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(265, 70);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Resolve!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Resolve";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // unresolvedBox
             // 
-            this.unresolvedBox.Location = new System.Drawing.Point(32, 72);
+            this.unresolvedBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.unresolvedBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.unresolvedBox.ForeColor = System.Drawing.Color.White;
+            this.unresolvedBox.Location = new System.Drawing.Point(34, 109);
             this.unresolvedBox.Name = "unresolvedBox";
             this.unresolvedBox.Size = new System.Drawing.Size(732, 20);
             this.unresolvedBox.TabIndex = 1;
@@ -56,7 +78,10 @@
             // 
             // resolvedBox
             // 
-            this.resolvedBox.Location = new System.Drawing.Point(32, 224);
+            this.resolvedBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.resolvedBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resolvedBox.ForeColor = System.Drawing.Color.White;
+            this.resolvedBox.Location = new System.Drawing.Point(34, 261);
             this.resolvedBox.Name = "resolvedBox";
             this.resolvedBox.Size = new System.Drawing.Size(732, 20);
             this.resolvedBox.TabIndex = 2;
@@ -64,36 +89,224 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 95);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 132);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Put the url here!";
+            this.label1.Text = "Put the URL here!";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // torrentLabel2
+            // 
+            this.torrentLabel2.AutoSize = true;
+            this.torrentLabel2.ForeColor = System.Drawing.Color.White;
+            this.torrentLabel2.Location = new System.Drawing.Point(31, 284);
+            this.torrentLabel2.Name = "torrentLabel2";
+            this.torrentLabel2.Size = new System.Drawing.Size(141, 13);
+            this.torrentLabel2.TabIndex = 4;
+            this.torrentLabel2.Text = "Magnet link comes out here!";
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.ForeColor = System.Drawing.Color.White;
+            this.exitButton.Image = ((System.Drawing.Image)(resources.GetObject("exitButton.Image")));
+            this.exitButton.Location = new System.Drawing.Point(750, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(50, 50);
+            this.exitButton.TabIndex = 0;
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.minimizeButton.FlatAppearance.BorderSize = 0;
+            this.minimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeButton.ForeColor = System.Drawing.Color.White;
+            this.minimizeButton.Image = ((System.Drawing.Image)(resources.GetObject("minimizeButton.Image")));
+            this.minimizeButton.Location = new System.Drawing.Point(700, 0);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(50, 50);
+            this.minimizeButton.TabIndex = 5;
+            this.minimizeButton.UseVisualStyleBackColor = false;
+            this.minimizeButton.Click += new System.EventHandler(this.minimizeButton_Click);
+            // 
+            // resolverButton
+            // 
+            this.resolverButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.resolverButton.FlatAppearance.BorderSize = 0;
+            this.resolverButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resolverButton.ForeColor = System.Drawing.Color.White;
+            this.resolverButton.Location = new System.Drawing.Point(0, 0);
+            this.resolverButton.Name = "resolverButton";
+            this.resolverButton.Size = new System.Drawing.Size(175, 50);
+            this.resolverButton.TabIndex = 6;
+            this.resolverButton.Text = "Bluemediafiles Resolver";
+            this.resolverButton.UseVisualStyleBackColor = false;
+            this.resolverButton.Click += new System.EventHandler(this.resolverButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.searchButton.FlatAppearance.BorderSize = 0;
+            this.searchButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.searchButton.ForeColor = System.Drawing.Color.Maroon;
+            this.searchButton.Location = new System.Drawing.Point(175, 0);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(175, 50);
+            this.searchButton.TabIndex = 7;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = false;
+            // 
+            // directdlButton
+            // 
+            this.directdlButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.directdlButton.FlatAppearance.BorderSize = 0;
+            this.directdlButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.directdlButton.ForeColor = System.Drawing.Color.DarkRed;
+            this.directdlButton.Location = new System.Drawing.Point(350, 0);
+            this.directdlButton.Name = "directdlButton";
+            this.directdlButton.Size = new System.Drawing.Size(175, 50);
+            this.directdlButton.TabIndex = 8;
+            this.directdlButton.Text = "Direct Download";
+            this.directdlButton.UseVisualStyleBackColor = false;
+            // 
+            // torrentButton
+            // 
+            this.torrentButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.torrentButton.FlatAppearance.BorderSize = 0;
+            this.torrentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.torrentButton.ForeColor = System.Drawing.Color.White;
+            this.torrentButton.Location = new System.Drawing.Point(525, 0);
+            this.torrentButton.Name = "torrentButton";
+            this.torrentButton.Size = new System.Drawing.Size(175, 50);
+            this.torrentButton.TabIndex = 9;
+            this.torrentButton.Text = "Torrent";
+            this.torrentButton.UseVisualStyleBackColor = false;
+            this.torrentButton.Click += new System.EventHandler(this.torrentButton_Click);
+            // 
+            // hestia
+            // 
+            this.hestia.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("hestia.BackgroundImage")));
+            this.hestia.Location = new System.Drawing.Point(791, 999);
+            this.hestia.Name = "hestia";
+            this.hestia.Size = new System.Drawing.Size(956, 465);
+            this.hestia.TabIndex = 10;
+            this.hestia.TabStop = false;
+            // 
+            // credits1
+            // 
+            this.credits1.AutoSize = true;
+            this.credits1.Font = new System.Drawing.Font("Noto Sans CJK SC Medium", 54.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.credits1.ForeColor = System.Drawing.Color.White;
+            this.credits1.Location = new System.Drawing.Point(-12, 6999);
+            this.credits1.Name = "credits1";
+            this.credits1.Size = new System.Drawing.Size(412, 108);
+            this.credits1.TabIndex = 11;
+            this.credits1.Text = "IGG Client";
+            // 
+            // credits2
+            // 
+            this.credits2.AutoSize = true;
+            this.credits2.Font = new System.Drawing.Font("Noto Sans CJK SC Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.credits2.ForeColor = System.Drawing.Color.White;
+            this.credits2.Location = new System.Drawing.Point(-509, 207);
+            this.credits2.Name = "credits2";
+            this.credits2.Size = new System.Drawing.Size(241, 28);
+            this.credits2.TabIndex = 12;
+            this.credits2.Text = "- With love, from Hestia!";
+            // 
+            // torrentUrl
+            // 
+            this.torrentUrl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.torrentUrl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.torrentUrl.ForeColor = System.Drawing.Color.White;
+            this.torrentUrl.Location = new System.Drawing.Point(34, 109);
+            this.torrentUrl.Name = "torrentUrl";
+            this.torrentUrl.Size = new System.Drawing.Size(732, 20);
+            this.torrentUrl.TabIndex = 13;
+            // 
+            // magnetLink
+            // 
+            this.magnetLink.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.magnetLink.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.magnetLink.ForeColor = System.Drawing.Color.White;
+            this.magnetLink.Location = new System.Drawing.Point(34, 261);
+            this.magnetLink.Name = "magnetLink";
+            this.magnetLink.Size = new System.Drawing.Size(732, 20);
+            this.magnetLink.TabIndex = 14;
+            // 
+            // torrentLabel1
+            // 
+            this.torrentLabel1.AutoSize = true;
+            this.torrentLabel1.ForeColor = System.Drawing.Color.White;
+            this.torrentLabel1.Location = new System.Drawing.Point(31, 132);
+            this.torrentLabel1.Name = "torrentLabel1";
+            this.torrentLabel1.Size = new System.Drawing.Size(193, 13);
+            this.torrentLabel1.TabIndex = 15;
+            this.torrentLabel1.Text = "Put IGG or PCGamesTorrent URL here.";
+            // 
+            // magnetgrabButton
+            // 
+            this.magnetgrabButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.magnetgrabButton.FlatAppearance.BorderSize = 0;
+            this.magnetgrabButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.magnetgrabButton.ForeColor = System.Drawing.Color.White;
+            this.magnetgrabButton.Location = new System.Drawing.Point(278, 334);
+            this.magnetgrabButton.Name = "magnetgrabButton";
+            this.magnetgrabButton.Size = new System.Drawing.Size(265, 70);
+            this.magnetgrabButton.TabIndex = 16;
+            this.magnetgrabButton.Text = "Grab Magnet";
+            this.magnetgrabButton.UseVisualStyleBackColor = false;
+            this.magnetgrabButton.Click += new System.EventHandler(this.magnetgrabButton_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 247);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(31, 284);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(145, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Resolved url comes out here!";
+            this.label2.Size = new System.Drawing.Size(156, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Resolved URL comes out here!";
             // 
             // IGGResolver
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(46)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.torrentLabel2);
+            this.Controls.Add(this.torrentUrl);
+            this.Controls.Add(this.credits2);
+            this.Controls.Add(this.torrentButton);
+            this.Controls.Add(this.directdlButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.resolverButton);
+            this.Controls.Add(this.minimizeButton);
+            this.Controls.Add(this.exitButton);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.resolvedBox);
             this.Controls.Add(this.unresolvedBox);
+            this.Controls.Add(this.credits1);
+            this.Controls.Add(this.hestia);
+            this.Controls.Add(this.magnetLink);
+            this.Controls.Add(this.resolvedBox);
+            this.Controls.Add(this.magnetgrabButton);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.torrentLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "IGGResolver";
-            this.Text = "IGG Link Resolver";
+            this.Text = "IGG Client";
             this.Load += new System.EventHandler(this.IGGResolver_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.IGGResolver_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.IGGResolver_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.hestia)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +318,20 @@
         private System.Windows.Forms.TextBox unresolvedBox;
         private System.Windows.Forms.TextBox resolvedBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label torrentLabel2;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button minimizeButton;
+        private System.Windows.Forms.Button resolverButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button directdlButton;
+        private System.Windows.Forms.Button torrentButton;
+        private System.Windows.Forms.PictureBox hestia;
+        private System.Windows.Forms.Label credits1;
+        private System.Windows.Forms.Label credits2;
+        private System.Windows.Forms.TextBox torrentUrl;
+        private System.Windows.Forms.TextBox magnetLink;
+        private System.Windows.Forms.Label torrentLabel1;
+        private System.Windows.Forms.Button magnetgrabButton;
         private System.Windows.Forms.Label label2;
     }
 }
--- IggClient/Form1.cs
+++ IggClient/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace IggClient
 {
@@ -15,20 +17,43 @@ namespace IggClient
         public IGGResolver()
         {
             InitializeComponent();
-            
+
+            // Repositioning //
+            hestia.Location = new Point(139, 56);
+            credits1.Location = new Point(12, 69);
+            credits2.Location = new Point(160, 161);
+
+            // Resolver Objects //
+            button1.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            unresolvedBox.Visible = false;
+            resolvedBox.Visible = false;
+
+            // Torrent Objects //
+            magnetgrabButton.Visible = false;
+            torrentLabel1.Visible = false;
+            torrentLabel2.Visible = false;
+            torrentUrl.Visible = false;
+            magnetLink.Visible = false;
+
+            // Credits Objects //
+            hestia.Visible = true;
+            credits1.Visible = true;
+            credits2.Visible = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             string unresolvedUrl = unresolvedBox.Text;
             string resolvedUrl = unresolvedUrl.Substring(unresolvedUrl.IndexOf('=') + 1);
-            if (unresolvedBox.Text.Contains("bluemedia"))
+            if (unresolvedBox.Text.Contains("bluemediafiles"))
             {
                 resolvedBox.Text = "http" + resolvedUrl;
             }
             else
             {
-                resolvedBox.Text = ("This is not an IGG link!");
+                resolvedBox.Text = ("This is not a bluemediafiles link!");
             }
 
         }
@@ -46,6 +71,143 @@ namespace IggClient
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void IGGResolver_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+
+        }
+        Point lastPoint;
+        private void IGGResolver_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void minimizeButton_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void resolverButton_Click(object sender, EventArgs e)
+        {
+            // Resolver Objects //
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            unresolvedBox.Visible = true;
+            resolvedBox.Visible = true;
+
+            // Torrent Objects //
+            magnetgrabButton.Visible = false;
+            torrentLabel1.Visible = false;
+            torrentLabel2.Visible = false;
+            torrentUrl.Visible = false;
+            magnetLink.Visible = false;
+
+            // Credits Objects //
+            hestia.Visible = false;
+            credits1.Visible = false;
+            credits2.Visible = false;
+        }
+
+        private void torrentButton_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            unresolvedBox.Visible = false;
+            resolvedBox.Visible = false;
+
+            // Torrent Objects //
+            magnetgrabButton.Visible = true;
+            torrentLabel1.Visible = true;
+            torrentLabel2.Visible = true;
+            torrentUrl.Visible = true;
+            magnetLink.Visible = true;
+
+            // Credits Objects //
+            hestia.Visible = false;
+            credits1.Visible = false;
+            credits2.Visible = false;
+        }
+
+        private void magnetgrabButton_Click(object sender, EventArgs e)
+        {
+            string torrentParsing = torrentUrl.Text;
+
+            if (torrentParsing.Contains("igg-games"))
+            {
+                WebRequest igggames = WebRequest.Create(torrentParsing);
+                WebResponse response = igggames.GetResponse();
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    string source = reader.ReadToEnd();
+                    if (source.Contains("Torrent Link</a>"))
+                    {
+                        string[] uno = (source.Split(new string[] { "Torrent Link</a>" }, StringSplitOptions.RemoveEmptyEntries));
+                        string[] dos = (uno[0].Split(new string[] { "TORRENT:" }, StringSplitOptions.RemoveEmptyEntries));
+                        string[] tres = (dos[1].Split(new string[] { "html" }, StringSplitOptions.RemoveEmptyEntries));
+                        string[] quatro = (tres[0].Split(new string[] { "http" }, StringSplitOptions.RemoveEmptyEntries));
+                        string secondurl = ("http" + quatro[1] + "html");
+
+                        WebRequest igggames2 = WebRequest.Create(secondurl);
+                        WebResponse response2 = igggames2.GetResponse();
+                        using (Stream dataStream2 = response2.GetResponseStream())
+                        {
+                            StreamReader reader2 = new StreamReader(dataStream2);
+                            string source2 = reader2.ReadToEnd();
+                            if (source2.Contains("magnet:?"))
+                            {
+                                string[] uno2 = (source2.Split(new string[] { "magnet:?" }, StringSplitOptions.RemoveEmptyEntries));
+                                string[] dos2 = (uno2[1].Split(new string[] { "Torrent</a>" }, StringSplitOptions.RemoveEmptyEntries));
+                                string[] tres2 = (dos2[0].Split(new string[] { "\">" }, StringSplitOptions.RemoveEmptyEntries));
+                                magnetLink.Text = ("magnet:?" + tres2[0]);
+                            }
+                            else
+                            {
+                                magnetLink.Text = ("The magnet link was not found on the torrent page, the problem is on IGG's side.");
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        magnetLink.Text = ("Torrent link not found or invalid URL.");
+                    }
+                }   
+            }
+            if (torrentParsing.Contains("pcgamestorrents"))
+            {
+                WebRequest pcgamestorrents = WebRequest.Create(torrentParsing);
+                WebResponse response = pcgamestorrents.GetResponse();
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    string source = reader.ReadToEnd();
+                    if (source.Contains("magnet:?"))
+                    {
+                        string[] uno2 = (source.Split(new string[] { "magnet:?" }, StringSplitOptions.RemoveEmptyEntries));
+                        string[] dos2 = (uno2[1].Split(new string[] { "Torrent</a>" }, StringSplitOptions.RemoveEmptyEntries));
+                        string[] tres2 = (dos2[0].Split(new string[] { "\">" }, StringSplitOptions.RemoveEmptyEntries));
+                        magnetLink.Text = ("magnet:?" + tres2[0]);
+                    }
+                    else
+                    {
+                        magnetLink.Text = ("The magnet link was not found on the torrent page, the problem is on IGG's side.");
+                    }
+                }
+            }
         }
     }
 }
